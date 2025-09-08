dc4b3775024c35cbd185d08a0a670cfab7477056
Bun go J-lou <bungoj-lou_bsit@plmun.edu.ph>
2025-09-08 16:24:59 +0800

    drop_db.sql

--- university_db/drop_db.sql
+++ university_db/drop_db.sql
@@ -1,3 +1,7 @@
+-- Step 3: (Optional) Drop the database if needed. 
 USE `UniversityDB`;
-DROP DATABASE `UniversityDB`;
+
+-- The IF EXISTS clause prevents an error if the database doesn't exist.
+DROP DATABASE IF EXISTS `UniversityDB`;
+
 
