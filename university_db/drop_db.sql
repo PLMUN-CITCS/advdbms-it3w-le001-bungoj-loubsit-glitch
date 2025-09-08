d0a74cc8a9731dc2ca4f33dc44997e8ed28a21bc
Bun go J-lou <bungoj-lou_bsit@plmun.edu.ph>
2025-09-08 16:17:06 +0800

    drop_db.sql

--- university_db/drop_db.sql
+++ university_db/drop_db.sql
@@ -1,15 +1,3 @@
-Enter password:
-Welcome to the MariaDB monitor.  Commands end with ; or \g.
-Your MariaDB connection id is 8
-Server version: 11.4.5-MariaDB mariadb.org binary distribution
+USE `UniversityDB`;
+DROP DATABASE `UniversityDB`;
 
-Copyright (c) 2000, 2018, Oracle, MariaDB Corporation Ab and others.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
-MariaDB [(none)]> USE `UniversityDB`;
-Database changed
-MariaDB [UniversityDB]> DROP DATABASE `UniversityDB`;
-Query OK, 0 rows affected (0.014 sec)
-
-MariaDB [(none)]>
